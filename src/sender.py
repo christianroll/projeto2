2d880d2f6743811946083f5038055c2071b22d4b
christianroll <christianr@comp.ufscar.br>
2015-12-03 18:09:36 -0200

    Sender inicial

--- src/sender.py
+++ src/sender.py
@@ -13,6 +13,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import argparse
 import sys
+import socket
+from util import crc32 as checksum
+import os
+
 
 __authors__ = (
                 'Christian Rollmann',
@@ -24,7 +28,51 @@ __license__ = "GPL v3"
 __version__ = "1.0"
 
 
+
+
+# MSS: Maximum segment size
+MSS = 2000
+
+
 def main(args):
+    try:
+        sdr_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sdr_sock.bind(("", args.port))
+        print "waiting on port:", port
+        data, addr = sdr_sock.recvfrom(MSS)
+        #TODO: verificacao do checksum
+        envia_ack(9001, num, host, porta)
+        filename = processa_pacote(data)
+        if (os.path.isfile(filename)):
+            # If the file exists, start receiving from sender
+            with open(filename + "_rcvd", mode="wd") as sdr_file:
+                pkt = cria_pacotes(sdr_file.read())
+            seq_num = 0
+            try:
+                while True:
+            
+            except socket.error:
+                print("Socket error")
+                continue
+            
+           
+
+
+
+
+
+
+        else:
+            print "Não existe arquivo"
+
+    except socket.error:
+        print("Failed to open UDP socket")
+                
+        
+        
+
+
+
     return 0
 
 
