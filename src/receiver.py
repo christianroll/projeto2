bc2f3b0e2b58c67803dae1181ca533853c603ffa
Julio Batista Silva <julio@juliobs.com>
2015-12-04 17:23:14 -0200

    Temp

--- src/receiver.py
+++ src/receiver.py
@@ -49,20 +49,11 @@ def main(args):
 
     # Request the file "filename" from Sender
     #envia_dados(args.filename, TIPO_NOME, rcv_sock, args.hostname, args.port, RCV_CWND)
+    rcv_sock.sendto(args.filename, (args.hostname, args.port))
 
-    filenam = args.filename
-    rcv_sock.sendto(filenam, (args.hostname, args.port))
-
-    # # If the file exists, start receiving from sender
-    # dados = recebe_dados(rcv_sock)
-    # printf("Dados: {}".format(dados))
-
-    
+    # If the file exists, start receiving from sender
     dados = recebe_dados(rcv_sock, args.hostname, args.port)
     print("dados: {}".format(dados))
-    bla = processa_pacote(dados)
-    print("Teste: {}".format(bla))    
-
 
     # pkt = Pacote(num_seq=0, chksum=0, tipo=0, data='')
     # dados = ''
--- src/util.py
+++ src/util.py
@@ -80,15 +80,15 @@ def processa_pacote(dado):
 
 # Envia pacote ACK
 def envia_ack(sock, num_seq, host, porta):
-    ack_pkt = Pacote(num_seq=num_seq, chksum=0, tipo=TIPO_ACK, data='')
+    ack_pkt = Pacote(num_seq=num_seq, chksum=0, tipo=TIPO_ACK, data=str())
     envia_um_pacote(sock, ack_pkt, host, porta)
 
 
 # Envia um pacote pelo socket
 def envia_um_pacote(sock, pkt, host, porta):
     dado = pack('IIH' + str(len(pkt.data)) + 's', pkt.num_seq, pkt.chksum, pkt.tipo, pkt.data)
+    print("Enviando um pacote. chksum: {}, tipo: {}, data: {}".format(pkt.chksum, pkt.tipo, pkt.data))
     sock.sendto(dado, (host, porta))
-    print("teste2")
 
 
 # Rotina para corromper o pacote
@@ -108,16 +108,17 @@ def envia_pacotes(sock, pacotes, host, porta, window):
         if sem_ack < window and (sem_ack + ultimo_sem_ack) < len(pacotes):
             envia_um_pacote(sock, pacotes[ultimo_sem_ack + sem_ack], host, 9002)
             sem_ack += 1
-            print("teste")
+            continue
         else:
             # Se a janela estiver cheia, ela espera os acks para esvaziar a janela.
             # Espera pelos acks timeout segundos
             pronto = select.select([sock], [], [], TIMEOUT)
             if pronto[0]:
                 dado, addr = sock.recvfrom(MSS)
+                print("Pronto")
             # Janela cheia e nenhum ACK recebido antes de timeout
             else:
-                print ("Timeout, seq num = {}".format(ultimo_sem_ack))
+                print ("Timeout. Seq num = {}".format(ultimo_sem_ack))
                 sem_ack = 0
                 continue
             # Confirma se o pacote é mesmo do servidor
@@ -135,22 +136,11 @@ def envia_pacotes(sock, pacotes, host, porta, window):
             else:
                 sem_ack = 0
 
-def envia_pacotes2(sock, pacotes, host, porta, window):
-    return null
-
-
 
 # Funcao que cria pacotes, envia os pacotes e manda fim de arquivo (EOF)
 def envia_dados(dados, tipo, sock, host, porta, window):
     envia_pacotes(sock, cria_pacotes(dados, tipo), host, porta, window)
-    # envia_um_pacote(cria_pacotes('', tipo=TIPO_EOF))
-    
-    fim = 'blabla'
-    fim2 = unicodedata.normalize('NFKD', fim).encode('ascii', 'ignore')
-    pacotefinal = cria_pacotes(fim2, TIPO_EOF)
-    print("pacotefinal: {}".format(pacotefinal[0]))
-    envia_um_pacote(sdr_sock, pacotefinal[0], '', 9002)
-
+    envia_um_pacote(cria_pacotes(str(), tipo=TIPO_EOF))
 
 
 # Funcao para receber dados
@@ -163,7 +153,6 @@ def recebe_dados(sock, host, porta):
         pkt = processa_pacote(data)
 
         print("received: {}".format(pkt))
-        print("\n")
 
         if pkt.tipo == TIPO_ACK:
             print("Ack received") 
