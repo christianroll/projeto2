32da5950a9ad505f4f1a6376be6ba786c3e47fe6
Marcelo Tedeschi <marcelotedeschi@hotmail.com>
2015-12-07 12:40:09 -0200

    Adicionando parametros via shell para o run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # O arquivo será salvo no mesmo diretorio do arquivo atual
 
+echo -n "Digite o tamanho da janela (ex: 10): "
+read cwnd
+echo -n "Digite a probabilidade de perda: "
+read pl
+echo -n "Digite a probabilidade de corrompimento: "
+read pc
+
+echo "You entered: $cwnd"
+
+
 [[ -z $(lsof -t -i :9001) ]] || kill $(lsof -t -i :9001)
 [[ -z $(lsof -t -i :9002) ]] || kill $(lsof -t -i :9002)
 
-python sender.py 10 0 0 -p 9001 & sleep 2
-python receiver.py 127.0.0.1 9001 ../LICENSE 0.4 0 &
+python sender.py $cwnd 0 $pc -p 9001 & sleep 2
+python receiver.py 127.0.0.1 9001 ../LICENSE $pl 0 &
 
 diff ../LICENSE ../LICENSE_rcvd
+
